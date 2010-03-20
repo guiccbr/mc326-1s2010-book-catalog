fcabb836ce348e296f2da3942f224faa2caeff30
gcc.ec09 <gcc.ec09@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-03-20 17:50:16 +0000

    Implementação de novas funções

---
+++ trunk/catalog.h
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <getopt.h>
+#include "books.h"
+#include <string.h>
+#include <stdio_ext.h>
+
+#define NOT_IMPLEMENTED fprintf(stderr, "This function has not been implemented yet. Wait for comming versions\n")
+#define HELP_FILE "help.dat"
+#define FILEALLOCERROR fprintf(stderr, "File or allocating problem.\n")
+#define NEXT_OPT (opt = getopt_long(argc, argv, short_opt, long_opt, &opt_index))
+#define CATNAME_SIZE 15
+#define INPUT_CLEAR __fpurge(stdin)
+#define MAXSTRING_SIZE 2000
+
+
+/*Prints Help from HelpFile if Helpfile exists, returning true. 
+* Otherwise returns false.
+*/
+bool printFile(const char* File);
+
+/*Creates an HTML list of books within CatalogFile if CatalogFile exists,
+* returning true. Otherwise, returns false.
+*/
+bool printList(const char* CatalogFile);
+
+/*Searches for keys in catalog*/
+bool query();
+
+/*Shows interactiveInterface, for runtime user's input*/
+void interactiveInterface();
+
+/*Prints a error message when dealing with invalid parameters*/
+void invalidParameter(int opt);
+
+/*Reads arguments from shell's program call*/
+void nonInteractiveInterface(int argc, char* argv[]);
+
+/*Checks if the file whose filename is the string pointed to by filename exists.
+* If it does, associates a stream with it. Otherwise, asks if the user wants
+* to create a new file.
+* Returns the pointer to the file stream-associated, or NULL, if no file was open.
+*/
+FILE * openFile(const char* filename, const char* mode);
+
+/*Checks if the file whose filename is the string pointed to by filename exist.
+* If it does, ask permission to subscribe it. Otherwise, creates a file named filename.
+* Returns NULL either if there's a problem allocating file or if permission to subscribe is denied.
+*/
+FILE * createFile(const char* filename, const char* mode);
+
---
+++ trunk/main.c
@@ -0,0 +1,43 @@
+#include "catalog.h"
+
+int main(int argc, char* argv[]) {
+	int opt_index; int opt;
+	const char* short_opt = "a:c:hiq:l:";
+	const struct option long_opt[] = {
+		{"create", 1, 0, 'c'},
+		{"help", 0, 0, 'h'},
+		{"interactive", 0, 0, 'i'},
+		{"query", 0, 0, 'q'},
+		{"list", 0, 0, 'l'},
+		{"add", 1, 0, 'a'},
+		{0,0,0,0}
+    };
+	    
+	switch (NEXT_OPT) {
+		case 'a':
+			nonInteractiveInterface(argc, argv);
+			break;
+		case 'c':
+		/*Funcao eh capaz de sobrescrever qualquer arquivo - o que eh extremamente inseguro
+		* TODO: Consertar isso*/
+			if(createFile(optarg, "a+"))
+				printf("'%s' Catalog created\n", optarg);
+			break;
+		case 'h':
+			printFile(HELP_FILE);
+			break;
+		case 'i':
+			interactiveInterface();
+			break;
+		case 'q':
+			query();
+			break;
+		case 'l':
+			printList(optarg);
+			break;
+		default:
+			fprintf(stderr, "-h for help. Parameters required\n");
+			break;	
+	}
+	exit (0);
+}
