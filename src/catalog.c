e183201a179506734dc04b81398586b1b9e301fe
guilherme.p.gonc <guilherme.p.gonc@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-03-22 00:41:20 +0000

    catalog.c: Added (a lot of) (unnecessary?) error checking to createFile

--- src/catalog.c
+++ src/catalog.c
@@ -2,11 +2,11 @@
 
 bool printFile(const char* File) {
 	FILE* f = fopen(File, "r");
-	char c;
+	int c;
 	if (f){
 		while((c = fgetc(f)) != EOF)
 			putchar(c);
-        fclose(f);
+		fclose(f);
 		return true;
 	}
 	return false;
@@ -23,20 +23,62 @@ bool printList(const char* CatalogFile) {
 }
 
 FILE * createFile(const char* filename, const char* mode) {
-	FILE * file = fopen(filename, "r");
-	char opt;
-	if(file != NULL){
-		puts("File already exists. Do you wish to overwrite it? (y/n)");
-        INPUT_CLEAR;
-		scanf("%c", &opt);
-		if(toupper(opt) == 'N'){
-			fprintf(stderr, "Unable to create new catalog\n");
-			return NULL;
-		}else{	
-         fclose(file);
-			remove(filename);
+		struct stat info;
+		char * basename;
+		char opt;
+		
+		/* TODO - All this verification code should be in a separate function */
+
+		if ( stat(filename, &info) == -1 ) {
+			/* TODO - Any other errors we should catch? */
+			switch (errno) {
+				case EACCES: /* No search permissions */
+				case EFAULT: /* Malformed path */
+					printf("Unable to open file: %s\n", strerror(errno));
+					break;
+				
+				case ENOENT: 
+					/* Some part of the path doesn't exist. */
+					
+					basename = strrchr(filename, '/');
+
+					if ( basename != NULL ) { /* Checking the dirs above the filename */
+						*basename = '\0';
+						
+						if ( stat(filename, &info) == -1 || (! S_ISDIR(info.st_mode))) {
+							/* Something's wrong with the path, nothing we can do */
+							fprintf(stderr, "Invalid catalog path: %s\n", filename);
+							return NULL;
+						}
+
+						/* Path exists, file doesn't. Create it. */
+						*basename = '/';
+					}	
+
+					/* else: filename doesn't exist and is relative to the current dir */
+			}
+	
+		} else { /* File already exists */
+
+			/* Catalogs should be regular files */
+			if (! S_ISREG(info.st_mode) ) {
+				fprintf(stderr, "Invalid catalog: %s\n", filename); 
+				return NULL;
+			}
+
+			puts("File already exists. Do you wish to overwrite it? (y/n)");
+			INPUT_CLEAR;
+			scanf("%c", &opt);
+
+			/* Do not overwrite unless explicitly allowed to do so */
+			if(toupper(opt) != 'Y'){
+				fprintf(stderr, "Unable to create new catalog\n");
+				return NULL;
+			}else{	
+				remove(filename);
+			}
 		}
-	}
+
 	return fopen(filename, mode);
 }
 
