de54bdf89ec07e7f48d25746fde6d198cd2aa574
gcc.ec09 <gcc.ec09@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-03-26 01:49:05 +0000

    Including new files, but not new implementations.

--- trunk/include/catalog.h
+++ trunk/include/catalog.h
@@ -3,16 +3,9 @@
 #include <getopt.h>
 #include "books.h"
 #include <string.h>
-#include <stdio_ext.h>
 
-#define NOT_IMPLEMENTED fprintf(stderr, "This function has not been implemented yet. Wait for comming versions\n")
-#define HELP_FILE "help.dat"
-#define FILEALLOCERROR fprintf(stderr, "File or allocating problem.\n")
-#define NEXT_OPT (opt = getopt_long(argc, argv, short_opt, long_opt, &opt_index))
+/*Definition for maximum catalog name string lenght*/
 #define CATNAME_SIZE 15
-#define INPUT_CLEAR __fpurge(stdin)
-#define MAXSTRING_SIZE 2000
-
 
 /*Prints Help from HelpFile if Helpfile exists, returning true. 
 * Otherwise returns false.
@@ -30,18 +23,8 @@ bool query();
 /*Shows interactiveInterface, for runtime user's input*/
 void interactiveInterface();
 
-/*Prints a error message when dealing with invalid parameters*/
-void invalidParameter(int opt);
-
 /*Reads arguments from shell's program call*/
 void nonInteractiveInterface(int argc, char* argv[]);
-
-/*Checks if the file whose filename is the string pointed to by filename exists.
-* If it does, associates a stream with it. Otherwise, asks if the user wants
-* to create a new file.
-* Returns the pointer to the file stream-associated, or NULL, if no file was open.
-*/
-FILE * openFile(const char* filename, const char* mode);
 
 /*Checks if the file whose filename is the string pointed to by filename exist.
 * If it does, ask permission to subscribe it. Otherwise, creates a file named filename.
--- trunk/include/tools.h
+++ trunk/include/tools.h
@@ -1,15 +1,26 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
 #include <string.h>
 #include <libgen.h>
+#include <stdio_ext.h>
 
 /* Definitions for file path validation */
 #define ERROR 0
 #define FILE_EXISTS 1
 #define DIR_EXISTS 2
+
+/* Extra tools definitions*/
+#define NOT_IMPLEMENTED fprintf(stderr, "This function has not been implemented yet. Wait for comming versions\n")
+#define NEXT_OPT (opt = getopt_long(argc, argv, short_opt, long_opt, &opt_index))
+#define FILEALLOCERROR fprintf(stderr, "File or allocating problem.\n")
+#define INPUT_CLEAR __fpurge(stdin)
+#define MAXSTRING_SIZE 2000
+#define HELP_FILE "help.dat"
 
 /* Validates a path to a regular file.
  * Receives: char * path - the file path
@@ -20,3 +31,13 @@
  * errno is set accordingly when ERROR is returned.
  */
 int validateFile(const char * path);
+
+/*Checks if the file whose filename is the string pointed to by filename exists.
+* If it does, associates a stream with it. Otherwise, asks if the user wants
+* to create a new file.
+* Returns the pointer to the file stream-associated, or NULL, if no file was open.
+*/
+FILE * openFile(const char* filename, const char* mode);
+
+/*Prints a error message when dealing with invalid parameters*/
+void invalidParameter(int opt);
