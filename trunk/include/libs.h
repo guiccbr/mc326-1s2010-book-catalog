24cd361d3a9ac543883845fa5166933f83a30ba2
gcc.ec09 <gcc.ec09@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-03-26 06:19:44 +0000

    libs.h, as discussed before.

---
+++ trunk/include/libs.h
@@ -0,0 +1,20 @@
+
+/*Standar Libraries*/
+#include <ctype.h>
+#include <errno.h>
+#include <getopt.h>
+#include <libgen.h>
+#include <stdbool.h>
+#include <stdio_ext.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*Built Libraries*/
+#include "books.h"
+#include "catalog.h"
+#include "tools.h"
+#include "menus.h"
