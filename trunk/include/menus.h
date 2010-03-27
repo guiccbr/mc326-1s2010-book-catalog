3d57adc560c37b5af9c575a71b432fe5eda60fdc
gcc.ec09 <gcc.ec09@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-03-27 01:40:39 +0000

    More Comments

--- trunk/include/menus.h
+++ trunk/include/menus.h
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------------------------*/
+
 /*Presents the main functions of the application, offering the possibility of running them.
 * Returns the code of the required function to run.
 *
@@ -12,17 +12,17 @@
 *
 */
 int mainMenu();
-/*-----------------------------------------------------------------------------------------*/
+
 /*Screen shows an organized HTML list of books from an user-chosen catalog
 * NOT FULLY IMPLEMENTED
 */
 void listprintMenu();
-/*-----------------------------------------------------------------------------------------*/
+
 /*Presents searching tools, offering user the possibility of running them
 * NOT FULLY IMPLEMENTED
 */
 void queryMenu();
-/*-----------------------------------------------------------------------------------------*/
+
 /*Asks for information (1-10) about a book inclusion:
 *
 * 1) Place (Catalog) of inclusion.
@@ -36,18 +36,18 @@ void queryMenu();
 * 9) Image File
 */
 void addBookMenu();
-/*-----------------------------------------------------------------------------------------*/
+
 /*Asks for path and name for a new catalog, creating it, if possible, and showing success message.
 * If there's a file allocating problem, raises an error, shows the user, and completely kills app.
 */
 void createCatalogMenu();
-/*-----------------------------------------------------------------------------------------*/
+
 /*Prints help*/
 void helpMenu();
-/*-----------------------------------------------------------------------------------------*/
+
 /*Gives the option of comming back to MainMenu after succesfully finalyzing a function execution*/
 int continueMenu();
-/*-----------------------------------------------------------------------------------------*/
+
 int tryagainMenu();
 bool tryAgainMenu();
 
