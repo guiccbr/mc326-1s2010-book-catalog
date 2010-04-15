e6b1cab1dfd3a4934073307794c25bb14b82b6cc
guilherme.p.gonc <guilherme.p.gonc@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-04-15 19:40:00 +0000

    Implemented (but haven't yet tested) fillISBNCatalog

--- trunk/include/catalog.h
+++ trunk/include/catalog.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdbool.h>
 
 #define BOOK_BLOCK_SIZE 100
@@ -15,12 +16,20 @@
  */
 #define getNextBook(bookptr, catalog) fread((bookptr), sizeof(Book), 1, (catalog))
 
-/* Reads a block of BOOK_BLOCK_SIZE from a catalog.
+/* Reads a block of BOOK_BLOCK_SIZE books from a catalog.
  * Receives: Book * bookptr - A book pointer (pointing to enough memory) that
  * 			      will store the data read
  * 	     FILE * catalog - The book catalog
  */
-#define readBlock(bookptr, catalog) fread((bookptr), sizeof(Book) * BOOK_BLOCK_SIZE, 1, (catalog))
+#define readBlock(bookptr, catalog) fread((bookptr), sizeof(Book), BOOK_BLOCK_SIZE, (catalog))
+
+/* Creates an index of ISBN fields and related corresponding position of
+ * registers in a catalog. Assumes both the catalog and index files exist.
+ * Receives: FILE * catalog - The catalog file from which data will be read.
+ *           FILE * index - The index file to be filled.
+ * Note: Fields are formatted as "ISBN RRN", one per line
+ */
+void fillISBNIndex(FILE * catalog, FILE * index);
 
 /*Creates an HTML list of books within CatalogFile if CatalogFile exists,
 * returning true. Otherwise, returns false.
