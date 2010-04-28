e4a097bef386fc499da277964e9fe8d7d3cf6f4c
gcc.ec09 <gcc.ec09@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-04-28 01:16:11 +0000

    Added -o options

--- include/catalog.h
+++ include/catalog.h
@@ -33,22 +33,26 @@
 /*Creates an HTML list of books within CatalogFile if CatalogFile exists,
  * returning true. Otherwise, returns false.
  */
-bool generateList(const char* CatalogName);
+bool generateList(char* CatalogName, char* HTMLlistName);
+
+bool nonInteractiveQuery(int argc, char * argv[]);
 
 /* Searches for primary keys in catalog
  * Receives:	char * catalogName - Name of the catalog user wants to find a book
- *				char * primaryKey - Search primary key
+ *		char * primaryKey - Search primary key
+ *		char * HTMLout - Name of the output file
+ *		char * HTMLModel - Name of the model file
  * Returns true on success, and false if key wasn't found or if a problem occurred.
  * Note: Displays error message.
  */
-bool query(char* catalogName, char* primaryKey);
+bool query(char* catalogName, char* primaryKey, char* HTMLout, char * HTMLModel);
 
 /* Generates an HTML page with book info based in model
  * Receives:	Book * bk - Struct Book with Book information.
  *				char * modelFile - HTML model, with special expressions to be replaced by info.
  * Returns true on success. False otherwise.
  */
-bool generateBookDescription(Book* bk, char* modelFile);
+bool generateBookDescription(Book* bk, char* modelFile, char* HTMLout);
 
 void appendHTMLCatalogList(FILE * list, char * isbn, char * title);
 
