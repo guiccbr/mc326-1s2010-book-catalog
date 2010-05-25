8a4fec084d53401e8f7c2575456c7184fe926136
guilherme.p.gonc <guilherme.p.gonc@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-05-25 00:27:28 +0000

    Reorganized search tests, added a Subject search test.

--- trunk/test/searchISBN.sh
+++ trunk/test/searchISBN.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Search tests.
+# ISBN Search test.
 
-# ISBN search.
 # Expected result: file test/TestOut.html will be created according to model default.html
 # with information from book "Jimmy Corrigan - O Menino Mais Esperto do Mundo".
 ./catalog -q catalogo00.dat -i 9788535915488 -m default.html -o test/TestOut.html
