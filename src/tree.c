4fb475f99862674d38b12f40765eb107ab96d1de
gcc.ec09@gmail.com <gcc.ec09@gmail.com@40333aba-2e24-11df-86d9-415ac6a66f2c>
2010-06-11 17:42:07 +0000

    Still implementing tasks. Commiting for backup.

--- src/tree.c
+++ src/tree.c
@@ -2,22 +2,23 @@
 
 treeNode * split(treeNode * root, FILE * tree) {
 	int last;
+	int center;
 	treeNode * newNode;
 
 	last = root->keysNo;
-	medium = last/2;
+	center = last/2;
 
 	newNode = mallocNode();
 	
 	/*Copies Keys and Pointers to the new Node*/
-	for(i = (medium + 1); i < last; i++) {
+	for(i = (center + 1); i < last; i++) {
 		newNode->key[i] = root->key[i];
 		newNode->p[i] = root->p[i];
 	}
 	
 	/*Sets end of nodes*/
-	root->keysNo = medium + 1;
-	newNode->keysNo = last - keysNo;
+	root->keysNo = center + 1;
+	newNode->keysNo = last - root->keysNo;
 
 	/*gets New Node RRN and checks leaf bit*/
 	newNode->rrn = getAvail(tree);
@@ -27,7 +28,9 @@ treeNode * split(treeNode * root, FILE * tree) {
 	}
 	
 	/*Writes modification to file*/
+	seekTree(tree, root->rrn);
 	writeNode(root, tree);
+	seekTree(tree, newNode->rrn);
 	writeNode(newNode, tree);
 
 	/*Returns new son*/
@@ -39,14 +42,14 @@ treeNode * mallocNode() {
 
 	node = malloc(sizeof(treeNode));	
 	if(!node) {
-		fprintf(stderr, "mallocNode: Allocating Problem!");
-		exit (1);
+		fprintf(stderr, "mallocNode: Memory Allocation Problem!\n");
+		return NULL;
 	}
 	node->keysNo = 0;
 	return node;
 }
 
-bool treeInsertKey(char * key, enum indexType keyType, int bookRRN, int rrnRRN, FILE * tree, FILE * rrnsFile) {
+bool treeInsertKey(char * key, enum indexType keyType, int rrnsRRN, FILE * tree, FILE * rrnsFile) {
 	int rootRRN;
 	int lastKey;
 	treeNode newSon;
@@ -55,11 +58,17 @@ bool treeInsertKey(char * key, enum indexType keyType, int bookRRN, int rrnRRN, 
 
 	
 	/*Gets root RRN and loads it to memory*/
-	getRoot(tree, &rootRRN);
-	root = getNodeRRN(rootRRN);
+	rewind(tree);
+	/*Jumps avail List*/
+	fscanf("%d", &rootRRN);
+	/*Reads root RRN*/
+	rootRRN = getRootRRN(tree);
+	seekTreeRRN(rootRRN);
+	/*Loads Root to memory*/
+	root = getNextNode(tree);
 	
 	/*Adds key, checking if root has been splitted*/
-	newSon = treeInsertKey_Rec(key, root, tree);
+	newSon = treeInsertKey_Rec(key, keyType, rrnsRRN, root, tree, rrnsFile);
 	
 	/*Creates a new root if newSon != NULL*/
 	if(newSon) {
@@ -77,6 +86,7 @@ bool treeInsertKey(char * key, enum indexType keyType, int bookRRN, int rrnRRN, 
 		newRoot->rrn = getAvail(tree);
 		setRootRRN(tree, newRoot->rrn);
 		/*Writes new Root to file*/
+		seekTreeRRN(newRoot->rrn);
 		writeNode(newRoot, tree);
 		free(newRoot);
 		free(newSon);
@@ -84,7 +94,7 @@ bool treeInsertKey(char * key, enum indexType keyType, int bookRRN, int rrnRRN, 
 	free(root);
 	return true;
 }
-int getRoot(FILE * tree) {
+int getRootRRN(FILE * tree) {
 	int rootRRN;
 	
 	rewind(tree);
@@ -92,24 +102,26 @@ int getRoot(FILE * tree) {
 	return rootRRN;
 }
 
-treeNode * treeInsertKey_Rec(char * key, enum IndexType keyType, int bookRRN, int rrnRRN, treeNode subRoot, FILE * tree) {
+treeNode * treeInsertKey_Rec(char * key, enum IndexType keyType, int rrnsRRN, treeNode subRoot, FILE * tree, FILE * rrnsFile) {
 	treeNode * newSon = NULL;
 	int nextSonRRN;
 	treeNode * nextSon;
 	
-	
+	/*If node is a leaf, inserts key in it*/
 	if( isleaf(subRoot) ) {
-		nodeInsertKey(key, subRoot, tree);
+		nodeInsertKey(key, rrnsRRN, keyType, subRoot, tree, rrnsFile);
 		/*If leaf is full after inserting, splits it*/
 		if(subRoot->keysNo == ORDER) {
 			newSon = split(subRoot, tree);
 		}
 	}
+	/*If node is not a leaf, finds the subtree where key must be inserted*/
 	else {
 		nextSonRRN = subRoot->p[nodeFindIndex(key, keyType, subRoot)];
 		/*TODO: Check if it's NULL because allocation is done here*/
-		nextSon = getNodeRRN(nextSonRRN);
-		newSon = treeInsertKey_Rec(key, nextSon, tree);
+		seekTreeRRN(tree, nextSonRRN);
+		nextSon = getNextNode(tree);
+		newSon = treeInsertKey_Rec(key, keyType, rrnsRRN, nextSon, tree, rrnsFile);
 		free(nextSon);
 		/* If newSon != NULL, son node was splitted
 		 * Needs to insert son in actual node
@@ -126,37 +138,63 @@ treeNode * treeInsertKey_Rec(char * key, enum IndexType keyType, int bookRRN, in
 	return newSon;
 }
 
-void nodeInsertKey(char * key, int bookRRN, int rrnRRN, enum indexType keyType, treeNode * node, FILE * tree, FILE * rrnsFILE) {
+void nodeInsertKey(char * key, int rrnsRRN, enum indexType keyType, treeNode * node, FILE * tree, FILE * rrnsFILE) {
 	int place;
-	int rrnRRN;
+	rrnNode RRN;
+	int (* cmp)();
 	int i = 0, j = 0;
-	
+
 	/*Finds place for next key*/
-	place = nodeFindIndex(node);
+	place = nodeFindIndex(key, keyType, node);
 
 	/*Moves keys and pointers if key doesn't exist*/
-	if(cmp(node->key[place]) != 0) {
+	if(strcmp(node->key[place], key) != 0) {
 		for(i = (place + 1); (i <= node->keysNo); i++) {
 			node->key[i] = node->key[i-1];
 			node->p[i] = node->p[i-1];
 		}
 		/*Points key to list of rrns in rrns FILE*/
-		node->p[place] = rrnRRN;
-			
-		
+		node->p[place] = rrnsRRN;
+		/*writes Modification*/
+		seekTreeRRN(tree, node->rrn);
+		writeNode(node, tree);
 	}
 	/*If key already exists, add a new rrn to rrns list in file*/
 	else {
-		seekRRN(rrnsFILE, node->p[place]);
-		
-		
-			
-	/*Moves keys to accomplish insertion*/
-	for(j = node->keysNo; j > i; j--) {
-		node->key[j] = node->key[j-1];
-		node->p[j] = node->p[j-1];
+		seekRrnRRR(rrnsFILE, rrnsRRN);
+		rrnNode = getNextRRN(rrnsFILE);
+		rrnNode->next[keyType] = node->p[place];
+		node->p[place] = rrnRRN;
+		/*Writes Modifications*/
+		seekTreeRRN(tree, node->rrn);
+		writeNode(node, tree);
+		seekRrnRRN(rrnsFILE, rrnsRRN);
+		writeRrn(rrnNode, rrnsFILE);
 	}
+}
+int nodeFindIndex(char * key, enum IndexType keyType, treeNode * node) {
+	int i = 0;
 
-	/*Inserts new key in it's correct position*/
+	switch (keyType) {
+		case TITLE: case AUTHOR: case SUBJECT:
+			cmp = compareWords();
+			break;
+		case YEAR:
+			cmp = compareYear();
+			break;
+		case ISBN:
+			cmp = compareISBN();
+			break;
+		default:
+			fprintf(stderr, "nodeInsertKey: 
+			Invalid indexType!\n -Aborting...\n");
+			exit(1);
+	}	
 	
+	/*Searches for the first key[i] >= key and stops counting*/ 
+	for(i = 0; (i < node->keysNo) && (cmp(key, node->key[i]) > 0); i++);
+	
+	return i;
+}
+
 	
